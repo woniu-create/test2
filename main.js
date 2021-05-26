f4e4faeb616ec65980a351afb53b890ae6486d7a
luyandan <2103029766@qq.com>
2021-05-27 00:45:44 +0800

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+let el = document.querySelector(".pop-up");
+let close = document.querySelector('.icon-close')
+console.log(close);
+console.log(el);
+function show(){
+    el.style.display = 'block';
+  }
+function hide(){
+    el.style.display = 'none'
+}
---
+++ px.js
@@ -0,0 +1,5 @@
+function pt(pt){
+ return px = (4*pt)/3
+}
+pt(70)
+console.log(px);
